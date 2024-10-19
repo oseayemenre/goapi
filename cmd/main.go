43f9bde1a4c00158feb3892cec5b0501ad4a92c0
Ayemenre Oseghale <oseayemenre04@gmail.com>
2024-10-19 15:24:29 +0100

    bootstrap server

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+type server struct {
+	addr string
+}
+
+func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello from the server"))
+}
+
+func main() {
+	s := &server{
+		addr: ":8080",
+	}
+
+	if err := http.ListenAndServe(s.addr, s); err != nil {
+		log.Fatal(err)	
+	}
+}
